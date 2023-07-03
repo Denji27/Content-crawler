7a5c7d76ba238a5d70d868b7b8730ada9617e3d3
Denji27 <duyanhaa1@gmail.com>
2023-07-03 15:08:34 +0700

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ContentCrawler contentCrawler = new ContentCrawler("https://dantri.com.vn/the-thao/sau-harry-kane-man-utd-tiep-tuc-tu-bo-kolo-muani-20230628153250878.htm");
+        contentCrawler.crawl();
+    }
+}
